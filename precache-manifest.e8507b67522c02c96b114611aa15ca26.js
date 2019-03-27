1f29a3591132f36d088ee5a37f88e8510171b78b
SimasDei <simondeiman@gmail.com>
2019-03-27 12:30:05 +0200

    Updates

---
+++ precache-manifest.e8507b67522c02c96b114611aa15ca26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0db3ca56a0cd082d1b2",
+    "url": "/static/css/main.922327e1.chunk.css"
+  },
+  {
+    "revision": "f0db3ca56a0cd082d1b2",
+    "url": "/static/js/main.87f6b573.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "87a33cb58a95ba3e8874",
+    "url": "/static/js/2.13ecabd2.chunk.js"
+  },
+  {
+    "revision": "d9eabb9a358b32abb0caf9ba44c97d32",
+    "url": "/index.html"
+  }
+];
