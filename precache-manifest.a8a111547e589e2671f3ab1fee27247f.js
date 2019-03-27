f7d64450a02afa02b540baca5205439ef7793fe3
SimasDei <simondeiman@gmail.com>
2019-03-27 12:35:10 +0200

    Updates

---
+++ precache-manifest.a8a111547e589e2671f3ab1fee27247f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2de1964d1d427696da2f",
+    "url": "/mediapark-task/static/css/main.922327e1.chunk.css"
+  },
+  {
+    "revision": "2de1964d1d427696da2f",
+    "url": "/mediapark-task/static/js/main.e4287672.chunk.js"
+  },
+  {
+    "revision": "928aafa7f4e97830a0c9",
+    "url": "/mediapark-task/static/js/runtime~main.ad1294d5.js"
+  },
+  {
+    "revision": "87a33cb58a95ba3e8874",
+    "url": "/mediapark-task/static/js/2.13ecabd2.chunk.js"
+  },
+  {
+    "revision": "c545e44b6e748cc8464656c9f5d41bc5",
+    "url": "/mediapark-task/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8507b67522c02c96b114611aa15ca26.js"
+  "/mediapark-task/precache-manifest.a8a111547e589e2671f3ab1fee27247f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/mediapark-task/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
